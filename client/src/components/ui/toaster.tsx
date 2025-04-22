cf0080b73d34e74247ee191f453c2e56802fd996
4ailabs <159195171+4ailabs@users.noreply.github.com>
2025-04-22 10:58:01 -0600

    Add minimal toaster component

---
+++ client/src/components/ui/toaster.tsx
@@ -0,0 +1,10 @@
+// Componente de tostadora simplificado solo para satisfacer las importaciones
+// En una implementación real, esto usaría un componente más sofisticado basado en shadcn/ui
+
+import React from "react";
+
+export function Toaster() {
+  // No hace nada en esta implementación mínima
+  // Estamos usando alert() nativo a través del hook use-toast
+  return null;
+}
